66641ea25e7fcddc5fc2552bde2fe9eb9a06b295
LaupWing <loc_strict@hotmail.com>
2023-08-02 20:12:14 +0200

    updated

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -9,7 +9,7 @@ type Viewport = "desktop" | "mobile"
 export function Header({ header, isLoggedIn, cart }: HeaderProps) {
    const { shop, menu } = header
    return (
-      <header className="flex container justify-between">
+      <header className="flex container justify-between mx-auto py-6">
          <NavLink prefetch="intent" to="/" style={activeLinkStyle} end>
             <strong>{shop.name}</strong>
          </NavLink>
