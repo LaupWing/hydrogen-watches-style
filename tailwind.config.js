7f45bf98a8c415be77d9716b37a203f434e9f322
LaupWing <loc_strict@hotmail.com>
2023-08-02 20:17:24 +0200

    udpated

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,12 @@ import typographyPlugin from "@tailwindcss/typography"
 /** @type {import('tailwindcss').Config} */
 export default {
    content: ["./app/**/*.{js,ts,jsx,tsx}"],
+   theme:{
+      extend: {
+         colors: {
+            background: "rgb(var(--background) / <alpha-value>)"
+         }
+      }
+   },
    plugins: [formsPlugin, typographyPlugin],
 }
