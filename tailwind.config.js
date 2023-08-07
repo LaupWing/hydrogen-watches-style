f052e17ac45ff5d31ff3d308adea8b7c428b5614
LaupWing <loc_strict@hotmail.com>
2023-08-07 19:11:04 +0200

    changed it to primary

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
    theme:{
       extend: {
          colors: {
-            background: "rgb(var(--background) / <alpha-value>)",
+            primary: "rgb(var(--primary) / <alpha-value>)",
             accent: "rgb(var(--accent) / <alpha-value>)"
          }
       }
